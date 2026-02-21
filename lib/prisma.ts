81de5b5f0ec1aedaf4132ad0a16b852448808b22
raelif <matteo.crielesi@gmail.com>
2026-02-21 15:52:56 +0100

    feat: prisma integration with next-auth

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,12 @@
-"use server";
+import "server-only";
 
-import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../app/generated/prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
 
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL
-});
-
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== "production") {
 	globalForPrisma.prisma = prisma;
